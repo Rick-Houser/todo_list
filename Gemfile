7ba03494001b4abb2292a65eaeeedcc99c1a00b9
FrackenToaster <frakentoaster@gmail.com>
2015-07-23 21:17:58 -0700

    Add todo item model and associated specs

--- Gemfile
+++ Gemfile
@@ -47,5 +47,6 @@ end
 
 group :test do 
 	gem 'capybara', '~>2.1.0'
+	gem 'shoulda-matchers','~>2.8.0'
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724002750) do
+ActiveRecord::Schema.define(version: 20150724041005) do
 
   create_table "to_do_lists", force: :cascade do |t|
     t.string   "title"
@@ -19,5 +19,14 @@ ActiveRecord::Schema.define(version: 20150724002750) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "todo_items", force: :cascade do |t|
+    t.integer  "todo_list_id"
+    t.string   "content"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
 
 end
