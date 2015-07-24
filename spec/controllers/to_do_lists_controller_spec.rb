72e80ca8bb15539a7f1ff366f6a858b46f0fd17e
FrackenToaster <frakentoaster@gmail.com>
2015-07-23 21:03:20 -0700

    Add specs for deleting todos

--- spec/controllers/to_do_lists_controller_spec.rb
+++ spec/controllers/to_do_lists_controller_spec.rb
@@ -23,7 +23,7 @@ describe ToDoListsController do
   # This should return the minimal set of attributes required to create a valid
   # ToDoList. As you add validations to ToDoList, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "title" => "MyString" } }
+  let(:valid_attributes) { { "title" => "MyString","description" => "MyDescription" } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
